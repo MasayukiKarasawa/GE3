e38dc799a57b0e05ffde748c5b0ca3e49317830e
MasayukiKarasawa <115062911+MasayukiKarasawa@users.noreply.github.com>
2023-10-03 17:07:28 +0900

    assertの定義まで

---
+++ Input.h
@@ -0,0 +1,12 @@
+#pragma once
+
+class Input
+{
+public:
+	//‰Šú‰»
+	void Initialize();
+	//XV
+	void Update();
+
+};
+
--- main.cpp
+++ main.cpp
@@ -11,6 +11,10 @@
 #include <dinput.h>
 #include <wrl.h>
 
+#include"Input.h"
+
+
+
 #pragma comment(lib, "d3d12.lib")
 #pragma comment(lib, "dxgi.lib")
 #pragma comment(lib, "d3dcompiler.lib")
@@ -215,6 +219,9 @@ LRESULT WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
+    Input* input_ = nullptr;
+
+
 #pragma region WindowsAPI初期化処理
     // ウィンドウサイズ
     const int window_width = 1280;  // 横幅
@@ -253,8 +260,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
     MSG msg{};  // メッセージ
 #pragma endregion
-
-#pragma region DirectX初期化処理
     // DirectX初期化処理　ここから
     HRESULT result;
     ComPtr<ID3D12Device> device;
@@ -468,22 +473,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
     // DirectX初期化処理　ここまで
 #pragma endregion
 
-    // DirectInputの初期化
-    ComPtr<IDirectInput8> directInput;
-    result = DirectInput8Create(
-        w.hInstance, DIRECTINPUT_VERSION, IID_IDirectInput8, (void**)&directInput, nullptr);
-    assert(SUCCEEDED(result));
-
-    // キーボードデバイスの生成
-    ComPtr<IDirectInputDevice8> keyboard;
-    result = directInput->CreateDevice(GUID_SysKeyboard, &keyboard, NULL);
-    // 入力データ形式のセット
-    result = keyboard->SetDataFormat(&c_dfDIKeyboard); // 標準形式
-    assert(SUCCEEDED(result));
-    // 排他制御レベルのセット
-    result = keyboard->SetCooperativeLevel(
-        hwnd, DISCL_FOREGROUND | DISCL_NONEXCLUSIVE | DISCL_NOWINKEY);
-    assert(SUCCEEDED(result));
+    input_ = new Input();
+    input_->Initialize();
+  
 
 #pragma region 描画初期化処理
 
@@ -1123,6 +1115,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
         // DirectX毎フレーム処理　ここまで
 
     }
+
+    delete input_;
 
     // ウィンドウクラスを登録解除
     UnregisterClass(w.lpszClassName, w.hInstance);
